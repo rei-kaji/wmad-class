d739e2ca52d39ae109b00e04e9a9bd13a0057f44
rei_kaji <100171821+rei-kaji@users.noreply.github.com>
2022-07-22 00:29:31 -0700

    Update problem1.py

--- assignment10/problem1.py
+++ assignment10/problem1.py
@@ -4,6 +4,7 @@
 # the function will provide an error message to the user and ask the user to enter another one. 
 # The user can enter numbers as long as s/he has not entered 0. Once a 0 is entered, the function 
 # returns the sum of all distinct numbers the user had entered. 
+# test
 
 def totalSumOfDistinctNumber():
 
